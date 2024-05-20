1de649ecab4d6407529ec35064fa5280bdfdea43
TheKit <thekit@disroot.org>
2024-05-20 23:33:32 +0300

    Make a hard link for _usrmerge tarballs instead to unbreak system-image

--- build.sh
+++ build.sh
@@ -155,8 +155,8 @@ else
     "$SCRIPT/build-tarball-mainline.sh" "${deviceinfo_codename}" "${OUT}" "${TMP}" "overlaystore"
 fi
 # compatibility symlink for  _usrmerge variant so that old pipelines just work
-ln -sf "device_${deviceinfo_codename}.tar.xz" "${OUT}/device_${deviceinfo_codename}_usrmerge.tar.xz"
-ln -sf "device_${deviceinfo_codename}.tar.build" "${OUT}/device_${deviceinfo_codename}_usrmerge.tar.build"
+ln -f "${OUT}/device_${deviceinfo_codename}.tar.xz" "${OUT}/device_${deviceinfo_codename}_usrmerge.tar.xz"
+ln -f "${OUT}/device_${deviceinfo_codename}.tar.build" "${OUT}/device_${deviceinfo_codename}_usrmerge.tar.build"
 
 if [ -z "$BUILD_DIR" ]; then
     rm -r "${TMP}"
